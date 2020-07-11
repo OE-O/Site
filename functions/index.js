0d8589e58c2c58309e90c3f67cf0693e267df052
Calum Henderson <calumproh28@gmail.com>
2020-07-11 20:17:54 +0100

    final

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,12 @@ const functions = require('firebase-functions');
 
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express('morgan');
 
-app.use(express.static('public'))
-app.use(morgan('dev'))
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(cors());
 
 //#region core#
 app.get("/", (req, res) => {
@@ -13,6 +15,13 @@ app.get("/", (req, res) => {
 });
 app.get("/legal", (req, res) => {
     res.sendFile(__dirname + "/views/legal.html")
+});
+
+app.get("/discord", (req, res) => {
+    res.redirect("https://discord.gg/byq9QE6");
+});
+app.get("/trello", (req, res) => {
+    res.redirect("https://trello.com/b/IP5aA43e/oe-o");
 });
 //#endregion
 
