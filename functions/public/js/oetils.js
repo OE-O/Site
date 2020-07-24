604b5390f7837d06b75170f2b321f637c08c7c71
Calum Henderson <calumproh28@gmail.com>
2020-07-24 12:58:12 +0100

    Added banner and replaced links to new application page.

--- functions/public/js/oetils.js
+++ functions/public/js/oetils.js
@@ -2,3 +2,10 @@ function easterEgg() {
     var x = document.getElementById('AudioEasterEgg');
     x.play();
 }
+async function closeBar() {
+    $('#hellobar-bar').fadeOut()
+    setTimeout(function () {
+        $('#main-content').attr('id', '');
+        $('#hellobar-bar').remove();
+    }, 400)
+}
