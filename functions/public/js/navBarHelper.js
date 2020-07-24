e9a44efb6b6bb50257665fe73d6c69557ad6d7b5
Calum Henderson <calumproh28@gmail.com>
2020-07-24 13:18:06 +0100

    Obfuscated Javascript

--- functions/public/js/navBarHelper.js
+++
@@ -1,8 +0,0 @@
-function c() {
-    if(window.outerWidth <= 604 || window.outerHeight <= 656) {
-        document.getElementById('noMobileNav').style = "display: none;";
-        return;
-    } else {
-        document.getElementById('noMobileNav').style = "margin-left: -10px";
-    }
-}
