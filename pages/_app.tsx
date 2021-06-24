66ca8f06a52f2d7fed6927b8d86a61f0218e37d0
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-06-24 20:49:27 +0100

    More import fixes and styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import { AppProps } from 'next/app';
 
-import { UserContext } from '@lib/context';
-import { useUserData } from '@lib/hooks';
-import Navbar from '@components/Navbar';
-import '@styles/globals.scss';
+import { UserContext } from 'lib/context';
+import { useUserData } from 'lib/hooks';
+import Navbar from 'components/Navbar';
+import 'styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
 	const userData = useUserData();
